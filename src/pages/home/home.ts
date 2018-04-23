aa0784baf3208b45fea55d5039adb2af418dad22
Rizal muhamad H <rizalmuhamadh@gmail.com>
2018-04-23 11:12:28 +0700

    Initial commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,29 @@
+import { DetailPage } from './../detail/detail';
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  items: any[];
+
+  constructor(public navCtrl: NavController) {
+    this.items = [];
+    for(let i = 0; i < 10; i++){
+      this.items.push({
+        text: 'item '+ i,
+        id: i
+      });
+    }
+  }
+
+  itemSelected(item){
+    this.navCtrl.push(DetailPage, {
+      item: item
+    });
+  }
+
+}
